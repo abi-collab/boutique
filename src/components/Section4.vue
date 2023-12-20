db6b049a3ad706ad2e4dd6833b570b66f61284cf
abiTolentino <josephreytolentino@gmail.com>
2023-12-20 21:48:06 +0800

    Update Section4.vue

--- src/components/Section4.vue
+++ src/components/Section4.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="grid grid-cols-4">
     <div v-for="image in images" :key="image.id" class="image-item overflow-hidden">
-      <img :src="image.url" alt="Image" class="image" />
+      <figure>
+         <img :src="image.url" alt="Image" class="image" />
+      </figure>
   </div>
   </div>
 </template>
@@ -11,14 +13,14 @@ export default {
   data() {
     return {
       images: [
-        { id: 1, url: 'src/assets/imgs/1.jpg' },
-        { id: 2, url: 'src/assets/imgs/2.jpg' },
-        { id: 3, url: 'src/assets/imgs/3.jpg' },
-        { id: 4, url: 'src/assets/imgs/4.jpg' },
-        { id: 1, url: 'src/assets/imgs/5.jpg' },
-        { id: 2, url: 'src/assets/imgs/6.jpg' },
-        { id: 3, url: 'src/assets/imgs/7.jpg' },
-        { id: 4, url: 'src/assets/imgs/8.jpg' },
+        { id: 1, url: '../assets/imgs/1.jpg' },
+        { id: 2, url: '../assets/imgs/2.jpg' },
+        { id: 3, url: '../assets/imgs/3.jpg' },
+        { id: 4, url: '../assets/imgs/4.jpg' },
+        { id: 1, url: '../assets/imgs/5.jpg' },
+        { id: 2, url: '../assets/imgs/6.jpg' },
+        { id: 3, url: '../assets/imgs/7.jpg' },
+        { id: 4, url: '../assets/imgs/8.jpg' },
       ],
     };
   },
