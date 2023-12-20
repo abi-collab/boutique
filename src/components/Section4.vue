5a442d5f2d264c0a587f6e505da120e9540ba4eb
abiTolentino <josephreytolentino@gmail.com>
2023-12-20 21:57:47 +0800

    Update Section4.vue

--- src/components/Section4.vue
+++ src/components/Section4.vue
@@ -1,24 +1,68 @@
 <template>
   <div class="grid grid-cols-4">
-    <div v-for="image in images" :key="image.id" class="image-item overflow-hidden">
+    <div class="image-item overflow-hidden">
       <figure>
-         <img :src="image.url" alt="Image" class="image" />
+         <img :src="image1" alt="Image" class="image" />
+      </figure>
+  </div>
+  <div class="image-item overflow-hidden">
+      <figure>
+         <img :src="image2" alt="Image" class="image" />
+      </figure>
+  </div>
+  <div class="image-item overflow-hidden">
+      <figure>
+         <img :src="image3" alt="Image" class="image" />
+      </figure>
+  </div>
+  <div class="image-item overflow-hidden">
+      <figure>
+         <img :src="image4" alt="Image" class="image" />
+      </figure>
+  </div>
+  <div class="image-item overflow-hidden">
+      <figure>
+         <img :src="image5" alt="Image" class="image" />
+      </figure>
+  </div>
+  <div class="image-item overflow-hidden">
+      <figure>
+         <img :src="image6" alt="Image" class="image" />
+      </figure>
+  </div>
+  <div class="image-item overflow-hidden">
+      <figure>
+         <img :src="image7" alt="Image" class="image" />
+      </figure>
+  </div>
+  <div class="image-item overflow-hidden">
+      <figure>
+         <img :src="image8" alt="Image" class="image" />
       </figure>
   </div>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
-     let images =  ref([
-        { id: 1, url: '../assets/imgs/1.jpg' },
-        { id: 2, url: '../assets/imgs/2.jpg' },
-        { id: 3, url: '../assets/imgs/3.jpg' },
-        { id: 4, url: '../assets/imgs/4.jpg' },
-        { id: 1, url: '../assets/imgs/5.jpg' },
-        { id: 2, url: '../assets/imgs/6.jpg' },
-        { id: 3, url: '../assets/imgs/7.jpg' },
-        { id: 4, url: '../assets/imgs/8.jpg' },
-      ]);
+import image1 from '../assets/imgs/1.jpg';
+import image2 from '../assets/imgs/2.jpg';
+import image3 from '../assets/imgs/3.jpg';
+import image4 from '../assets/imgs/4.jpg';
+import image5 from '../assets/imgs/5.jpg';
+import image6 from '../assets/imgs/6.jpg';
+import image7 from '../assets/imgs/7.jpg';
+import image8 from '../assets/imgs/8.jpg';
+
+// import { ref } from 'vue';
+//      let images =  ref([
+//         { id: 1, url: '../assets/imgs/1.jpg' },
+//         { id: 2, url: '../assets/imgs/2.jpg' },
+//         { id: 3, url: '../assets/imgs/3.jpg' },
+//         { id: 4, url: '../assets/imgs/4.jpg' },
+//         { id: 1, url: '../assets/imgs/5.jpg' },
+//         { id: 2, url: '../assets/imgs/6.jpg' },
+//         { id: 3, url: '../assets/imgs/7.jpg' },
+//         { id: 4, url: '../assets/imgs/8.jpg' },
+//       ]);
 
 </script>
