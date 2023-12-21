7848f9d72c8fe3296a022c54f7c38f2198421d42
abiTolentino <josephreytolentino@gmail.com>
2023-12-21 08:37:06 +0800

    Update Section1.vue

--- src/components/Section1.vue
+++ src/components/Section1.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="section1 grid place-items-center h-3/4">
+  <div class="section1 grid place-items-center h-1/2 md:h-screen">
     <div class="">
       <div id="TitleHead" class="text-white text-[100px] xs:text-[100px] lg:text-[150px] drop-shadow-2xl my-36">Passion Craft</div>
     <button class="py-4 px-12 tracking-wider rounded-full my-8 text-xl font-bold text-white bg-black bg-opacity-80 hover:bg-opacity-90 active:text-black active:bg-white">ORDER NOW</button>
